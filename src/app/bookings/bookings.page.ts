fe3dbe3cf7df9978066ba614975d570ff50a347d
mihocsaszilard <mihocsa48@gmail.com>
2021-12-13 14:01:42 +0100

    added cancel booking

--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IonItemSliding } from '@ionic/angular';
 import { Booking } from './booking.model';
 import { BookingService } from './booking.service';
 
@@ -11,10 +12,14 @@ export class BookingsPage implements OnInit {
 
   loadedBookings: Booking[];
 
-  constructor(private bookingsService: BookingService) { }
+  constructor(private bookingService: BookingService) { }
 
   ngOnInit() {
-    this.loadedBookings = this.bookingsService.getBookings;
+    this.loadedBookings = this.bookingService.getBookings;
   }
 
+  onCancelBooking(offerId: string, slidingEl: IonItemSliding) {
+    slidingEl.close();
+    // cancel booking with offerId
+  }
 }
