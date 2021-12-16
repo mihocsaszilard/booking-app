1278235efd65e00d5f20349cda966ec72fb7fda9
mihocsaszilard <mihocsa48@gmail.com>
2021-12-16 21:03:55 +0100

    filter all / bookable places

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
+import { SegmentChangeEventDetail } from '@ionic/core';
+import { Subscription } from 'rxjs';
+
+import { AuthService } from 'src/app/auth/auth.service';
 import { Place } from '../places.model';
 import { PlacesService } from '../places.service';
 
@@ -8,18 +12,56 @@ import { PlacesService } from '../places.service';
   templateUrl: './discover.page.html',
   styleUrls: ['./discover.page.scss'],
 })
-export class DiscoverPage implements OnInit {
+export class DiscoverPage implements OnInit, OnDestroy {
 
   discoverPlaces: Place[];
+  listedLoadedPlaces: Place[];
+  relevantPlaces: Place[];
+  isLoading = false;
+  private placesSub: Subscription;
 
-  constructor(private placesService: PlacesService, private menuCtrl: MenuController) { }
+  constructor(
+    private placesService: PlacesService,
+    private menuCtrl: MenuController,
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
-    this.discoverPlaces = this.placesService.getPlaces;
+    this.placesSub = this.placesService.getPlaces.subscribe(places => {
+      this.discoverPlaces = places;
+      this.relevantPlaces = this.discoverPlaces;
+      this.listedLoadedPlaces = this.relevantPlaces.slice(1);
+    });
+  }
+
+  ionViewWillEnter() {
+    this.isLoading = true;
+    this.placesService.fetchPlaces().subscribe(() => {
+      this.isLoading = false;
+    });
   }
 
   // open menu programatically
   // onOpenMenu() {
   //   this.menuCtrl.toggle();
   // }
+
+  onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
+    console.log(event.detail);
+    if (event.detail.value === 'all') {
+      this.relevantPlaces = this.discoverPlaces;
+      this.listedLoadedPlaces = this.relevantPlaces.slice(1);
+    } else {
+      this.relevantPlaces = this.discoverPlaces.filter(
+        place => place.userId !== this.authService.getUserId
+      );
+      this.listedLoadedPlaces = this.relevantPlaces.slice(1);
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.placesSub) {
+      this.placesSub.unsubscribe();
+    }
+  }
 }
